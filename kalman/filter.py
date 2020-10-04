d7e010eef9d5db345b3a0c5b8c51f06305428ca2
Vasilii Ramadanov <finimpres@gmail.com>
2020-10-04 12:31:24 +0300

    Add Extended Klaman Filter
    - add EKF class
    - move general elements from KF and EKF to
    the  __filter class

--- kalman/filter.py
+++ kalman/filter.py
@@ -1,38 +1,40 @@
 #!/usr/bin/env python3
 """
-This file decsribes the linear Kalman Filter algorithm
+This file contains different variations of the Kalman Filter algorithm
+__filter - base class, all filters should be inherited from this class
+KF  - implements the usual linear Kalman filter
+EKF - implements the extended Kalman filter
 """
 
 import numpy as np
 
 
-class KF:
+class __filter:
 
-    def __init__(self, transition_matrix, observation_matrix, covariance_matrix, process_covariance,
-                 observation_covariance, initial_state=None):
-        """Constructor of the Kalman filter"""
-        n = transition_matrix.shape[0]
-        m = observation_matrix.shape[0]
-
-        assert transition_matrix.shape == (n, n)
-        assert observation_matrix.shape == (m, n)
-        assert observation_covariance.shape == (m, m)
-        assert covariance_matrix.shape == (n, n)
-        assert process_covariance.shape == (n, n)
-
-        self.F = transition_matrix
-        self.H = observation_matrix
+    def __init__(self, covariance_matrix, process_covariance, observation_covariance, initial_state=None):
+        """Construct base filter"""
         self.P = covariance_matrix
         self.Q = process_covariance
         self.R = observation_covariance
 
         self.x = initial_state
 
+
+class KF(__filter):
+
+    def __init__(self, transition_matrix, observation_matrix, covariance_matrix, process_covariance,
+                 observation_covariance, initial_state=None):
+        """Constructor of the Kalman filter"""
+        super(KF, self).__init__(covariance_matrix, process_covariance, observation_covariance, initial_state)
+        
+        self.F = transition_matrix
+        self.H = observation_matrix
+
     def init(self, initial_state):
         self.x = initial_state
 
     def update(self, y):
-        """Implements Kalman filter logic"""
+        """Implements Kalman filter step"""
         F, H, P, Q, R = self.F, self.H, self.P, self.Q, self.R
         x = self.x
 
@@ -42,6 +44,37 @@ class KF:
         K = P @ H.T @ np.linalg.inv(H @ P @ H.T + R)
         x_hat = x_est + K @ (y - H @ x_est)
         tmp = K @ H
+        P = (np.eye(tmp.shape[0]) - tmp) @ P
+
+        self.x = x_hat
+        self.P = P
+
+
+class EKF(__filter):
+
+    def __init__(self, transition, observation, covariance_matrix, process_covariance,
+                 observation_covariance, initial_state=None):
+        """Constructor of the Extended Kalman filter"""
+        super(EKF, self).__init__(covariance_matrix, process_covariance, observation_covariance, initial_state)
+        self.F, self.Jf = transition
+        self.H, self.Jh = observation
+
+    def init(self, initial_state):
+        self.x = initial_state
+
+    def update(self, y):
+        """Implements Extended Kalman filter step"""
+        F, H, P, Q, R = self.F, self.H, self.P, self.Q, self.R
+        x = self.x
+
+        x_est = F(x)
+        Fk = self.Jf(x_est)
+        P = Fk @ P @ Fk.T + Q
+
+        Hk = self.Jh(x_est)
+        K = P @ Hk.T @ np.linalg.inv(Hk @ P @ Hk.T + R)
+        x_hat = x_est + K @ (y - H @ x_est)
+        tmp = K @ Hk
         P = (np.eye(tmp.shape[0]) - tmp) @ P
 
         self.x = x_hat
